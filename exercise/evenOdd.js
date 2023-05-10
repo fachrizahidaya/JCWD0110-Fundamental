9f4b8451890f2c2afa45ee6ececb13e7010fd38e
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-05-10 17:53:16 +0700

    updated

--- exercise/evenOdd.js
+++ exercise/evenOdd.js
@@ -1,11 +1,21 @@
 // if-else
 let number = 105;
-if (number % 2 == 0) {
-  console.log("Even");
-} else {
-  console.log("Odd");
+// if (number % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+let z = 5;
+for (let j = 0; j <= z; j++) {
+  console.log(j)
+  if (j % 2 == 0) {
+    console.log("even number");
+  } else {
+    console.log("odd number");
+  }
 }
 
 // ternary
 let result = number % 2 == 0 ? "even" : "odd ";
-console.log(`The number is ${result}`);
+// console.log(`The number is ${result}`);
