44a53869fe8f1f1c5b2b356204ef21e200876792
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-05-12 16:29:13 +0700

    updated

--- array.js
+++ array.js
@@ -1,68 +1,71 @@
 let arr = [];
 let arr1 = new Array();
-// console.log(arr);
-// console.log(arr1);
+console.log(arr);
+console.log(arr1);
 
 let arr2 = ["A", "B", "C", "D"];
 // access data in array
-// console.log(arr2[1]);
+console.log(arr2[1]);
 // replace data in array
 arr2[3] = "E";
-// console.log(arr2);
+console.log(arr2);
 
 // built-in
+
 // join
 let arr3 = ["A", "B", "C", "D"];
-// console.log(arr3.join());
+console.log(arr3.join());
+
 // pop
 let arr4 = ["A", "B", "C", "D"];
-// console.log(arr4.pop());
-// console.log(arr4);
+console.log(arr4.pop());
+console.log(arr4);
 
 // push
 let arr5 = ["A", "B", "C", "D"];
 arr5.push("F");
-// console.log(arr5);
+console.log(arr5);
 
 // shift
 let arr6 = [1, 2, 4, 5];
 arr6.shift();
-// console.log(arr6);
+console.log(arr6);
 
 // unshift
 let arr7 = [1, 2, 4, 5];
 arr7.unshift(3, 6);
-// console.log(arr7);
-// console.log(arr7.length);
+console.log(arr7);
+console.log(arr7.length);
 
 // slice
 let arr8 = [1, 2, 4, 5, 6];
-// console.log(arr8.slice(1, 4));
+console.log(arr8.slice(1, 4));
 
 // sort
 let arr9 = [5, 4, 6];
-// console.log(arr9.sort((a, b) => a - b)); //ascending
-// console.log(arr9.sort((a, b) => b - a)); // descending
+console.log(arr9.sort((a, b) => a - b)); //ascending
+console.log(arr9.sort((a, b) => b - a)); // descending
 
 // splice
 const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 fruits.splice(0, 2, "Durian", "Salak");
-// console.log(fruits);
+console.log(fruits);
 
 // foreach
-// let arr10 = [1, 2, 3, 4, 5];
-// let b = arr10.forEach((item, index) => {
-//   console.log(item);
-// });
-// console.log(b);
+let arr10 = [1, 2, 3, 4, 5];
+let b = arr10.forEach((item, index) => {
+  console.log(item);
+});
+console.log(b);
+// tidak bisa dipanggil karena hanya menampilkan saja,
 
-// // map
-// let arr11 = [1, 2, 3, 4, 5];
-// let a = arr11.map((item, index) => {
-//   return item * 2;
-// });
+// map
+let arr11 = [1, 2, 3, 4, 5];
+let a = arr11.map((item, index) => {
+  return item * 2;
+});
 
-// console.log(a);
+console.log(a);
 
 // for-of
 let fruits1 = ["apple", "orange", "plum"];
--- function.js
+++ function.js
@@ -1,114 +1,114 @@
 // function declaration
-// function square(n) {
-//   return n * n;
-// }
-// let x = square(4);
-// console.log(x);
+function square(n) {
+  return n * n;
+}
+let x = square(4);
+console.log(x);
 
 // function expression
-// let sum = function (n) {
-//   return n + n;
-// };
-// let a = sum(4);
-// console.log(a);
+let sum = function (n) {
+  return n + n;
+};
+let a = sum(4);
+console.log(a);
 
 // arrow function
-// let greeting = () => {
-//   console.log("hello");
-// };
-// greeting();
+let greeting = () => {
+  console.log("hello");
+};
+greeting();
 
 // example
-// let operational = (n) => {
-//   console.log(n * n);
-//   return n;
-// };
+let operational = (n) => {
+  console.log(n * n);
+  return n;
+};
 
-// let y = operational(3);
-// console.log(y);
+let y = operational(3);
+console.log(y);
 
 // scope
-// function salam() {
-//   let hello = "hello there";
-//   return hello;
-// }
+function salam() {
+  let hello = "hello there";
+  return hello;
+}
 
-// console.log(hello);
+console.log(hello);
 
 // parameter & argument
-// function getMessage(name, name2, name3) {
-//   const hello = "Hello";
-//   return hello + " " + name + " dan " + name2;
-// }
-// console.log(getMessage("David", "Joko"));
+function getMessage(name, name2, name3) {
+  const hello = "Hello";
+  return hello + " " + name + " dan " + name2;
+}
+console.log(getMessage("David", "Joko"));
 
 // default parameter
-// let multiply = (a, b = 2) => {
-//   return a * b;
-// };
-// console.log(multiply(5));
+let multiply = (a, b = 2) => {
+  return a * b;
+};
+console.log(multiply(5));
 
-// // rest parameter
-// function myFunc(a, b, ...manyMore) {
-//   console.log("a:", a);
-//   console.log("b:", b);
-//   console.log("many more:", manyMore);
-// }
+// rest parameter
+function myFunc(a, b, ...manyMore) {
+  console.log("a:", a);
+  console.log("b:", b);
+  console.log("many more:", manyMore);
+}
 
-// myFunc(1, 2, 3, 4, 5, 6, 7, 8);
+myFunc(1, 2, 3, 4, 5, 6, 7, 8);
 
 // nested fuction
-// function getMessage1(firstName) {
-//   let message4 = "hi there";
-//   function sayHello() {
-//     let message3 = "hi there";
-//     // console.log(message4);
-//     return "Hello" + " " + firstName + ",";
-//   }
+function getMessage1(firstName) {
+  let message4 = "hi there";
+  function sayHello() {
+    let message3 = "hi there";
+    // console.log(message4);
+    return "Hello" + " " + firstName + ",";
+  }
 
-//   function welcomeMessage() {
-//     return " Welcome to Purwadhika";
-//   }
-//   //   console.log(message4);
-//   return sayHello() + "" + welcomeMessage();
-// }
+  function welcomeMessage() {
+    return " Welcome to Purwadhika";
+  }
+  console.log(message4);
+  return sayHello() + "" + welcomeMessage();
+}
 
-// let message = getMessage1("David");
-// console.log(message);
+let message = getMessage1("David");
+console.log(message);
 
 // closure
-// let greeting1 = (name) => {
-//   let defaultMessage = "Hello";
-//   return function () {
-//     return defaultMessage + " " + name;
-//   };
-// };
-// let greetingDavid = greeting1("David");
-// console.log(greetingDavid());
+let greeting1 = (name) => {
+  let defaultMessage = "Hello";
+  return function () {
+    return defaultMessage + " " + name;
+  };
+};
+let greetingDavid = greeting1("David");
+console.log(greetingDavid());
 
 // currying
-// function multiplier(num1) {
-//   return function (num2) {
-//     return num2 * num1;
-//   };
-// }
-// const mul3 = multiplier(3);
-// console.log(mul3(5));
+function multiplier(num1) {
+  return function (num2) {
+    return num2 * num1;
+  };
+}
+const mul3 = multiplier(3);
+console.log(mul3(5));
 
 // currying
-// let preCurrying = (n1, n2) => {
-//   return n2 - n1;
-// };
-// console.log(preCurrying(3, 5));
+let preCurrying = (n1, n2) => {
+  return n2 - n1;
+};
+console.log(preCurrying(3, 5));
 //
 //
 //
-// let postCurrying = (n1) => {
-//   return function (n2) {
-//     return n2 - n1;
-//   };
-// };
-// console.log(postCurrying(3)(5));
+let postCurrying = (n1) => {
+  return function (n2) {
+    return n2 - n1;
+  };
+};
+console.log(postCurrying(3)(5));
 
 // recursive
 function countDown(fromNumber) {
@@ -124,8 +124,8 @@ countDown(3);
 // predefined function
 
 // isNaN()
-let a = "55";
-console.log(isNaN(a));
+let a1 = "55";
+console.log(isNaN(a1));
 // parseInt()
-console.log(parseInt(a));
-console.log(typeof parseInt(a));
+console.log(parseInt(a1));
+console.log(typeof parseInt(a1));
