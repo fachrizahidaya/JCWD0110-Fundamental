760cf210026504d69083428dd1899702c2ce69e2
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-05-19 09:25:43 +0700

    recursion

--- recursion.js
+++ recursion.js
@@ -1,9 +1,19 @@
 const factorial = (n) => {
-  if (n === 0) {
+  if (n <= 0) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    let nextNumber = n - 1;
+    return n * factorial(nextNumber);
   }
 };
-let num = 3;
+
+let num = 4; // 3 * 2 * 1
 console.log(factorial(num));
+
+// ternary operator
+
+const factorial2 = (n) => {
+  return n <= 0 ? 1 : n * factorial(n - 1);
+};
+
+console.log(factorial2(num));
