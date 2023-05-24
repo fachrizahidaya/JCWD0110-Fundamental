dc30c78c4f3e61d7aa6e2b4add6b83159cc2e97b
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-05-24 13:11:01 +0700

    update exercise

---
+++ findSingle.js
@@ -0,0 +1,15 @@
+function findSingle(arr) {
+  let single = [];
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    temp = arr.filter((a) => a == arr[i]);
+    console.log(temp);
+
+    if (temp.length == 1) {
+      single.push(arr[i]);
+    }
+  }
+  return single.join();
+}
+
+console.log(findSingle([4, 1, 2, 1, 2]));
